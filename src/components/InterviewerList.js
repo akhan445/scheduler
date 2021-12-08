615df66d1d97800560fc57ed24a2c3dc080f1548
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-12-08 16:27:10 -0500

    Refactor InterviewList to display InterviewListItems dynamically

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
+  // Creates a InterviewerListItem component for each interviewer from the list of interviewers
+  const interviewerListItemsArray = props.interviewers.map((currInterviewer) => {
+    return (
+      <InterviewerListItem
+        key={currInterviewer.id}
+        id={currInterviewer.id}
+        name={currInterviewer.name}
+        avatar={currInterviewer.avatar}
+        selected={props.interviewer === currInterviewer.id}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewerListItemsArray}
+      </ul>
     </section>
   );
 };
