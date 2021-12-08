e128f0695cf4713b57e59daadd4a83f8d4a227fa
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-12-08 00:10:41 -0500

    Add code to render DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  // formats the text for interview spots remaining
+  function formatSpots(spots) {
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    } else if (spots === 0) {
+      return `no spots remaining`;
+    }
+    return `1 spot remaining`;
+  }
+  const dayClass = classNames(
+    "day-list__item",
+    {"day-list__item--selected": props.selected},
+    {"day-list__item--full": props.spots === 0}
+  );
+  
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
