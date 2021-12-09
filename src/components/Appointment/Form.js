5aa997296784b84d5e82aba4177d8c356c705e26
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-12-08 22:07:43 -0500

    Add Form handling for Cancel button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,10 +6,25 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  // reset the values of the state hook
+  function reset() {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  /**
+   * function called when user cancels, resets the values of the state
+   * and then call the onCancel function passed through props 
+   */
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -27,7 +42,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
