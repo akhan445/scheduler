6d8daf1f7bcca25920bcbf0c2056c074e1241b19
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-12-08 20:17:01 -0500

    Add Empty component for Appointment

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,10 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
 
 // ==============         Button Component Stories         ============== //
 storiesOf("Button", module)
@@ -147,4 +150,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
