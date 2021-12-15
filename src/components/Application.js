b447763ee5d3f42a1e0db607b98d046a0c1b1a41
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-12-15 13:59:33 -0500

    Add custom hook useApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "components/Application.scss";
 
@@ -7,65 +6,16 @@ import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-    
-  // makes a get request to get the list of days from server
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
-  }, []);
 
-  // add new appointment
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, {interview})
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        });
-      });
-  }
-
-  // delete an appointment
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id] : appointment
-    }
-
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        });
-      });
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   // set the daily appointment and interviewers schedule by fetching the appointments by day and mapping each to Appointment component
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -84,9 +34,6 @@ export default function Application(props) {
       />
     );
   });
-
-  //function which changes the state of the current day
-  const setDay = day => setState({...state, day});
   
   return (
     <main className="layout">
